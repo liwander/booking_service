7f01664f187be65615faf41a11846965e9beddb7
Anver Gadzhiev <gadzhiev.anver@gmail.com>
2026-06-21 13:53:21 +0300

    fix imports

--- app/models/timeslots.py
+++ app/models/timeslots.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBasem, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, ForeignKey
+from datetime import datetime
+from typing import Optional
 
 class Base(DeclarativeBase):
     pass
@@ -11,5 +13,5 @@ class TimeSlot(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     start_time: Mapped[datetime]
     end_time: Mapped[datetime]
-    is_booked : Mapped[bool]
-    booked_by : Mapped[Optional[str]] = mapped_column(String(30))
+    is_booked : Mapped[bool] = mapped_column(default=False)
+    booked_by : Mapped[Optional[str]] = mapped_column(String(30), nullable=True)
