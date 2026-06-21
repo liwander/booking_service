b6f405537dba3162ca46227518c43cd16e42ee87
Anver Gadzhiev <gadzhiev.anver@gmail.com>
2026-06-21 11:57:16 +0300

    update/delete functionality

--- app/api/v1/time_slots.py
+++ app/api/v1/time_slots.py
@@ -1,6 +1,7 @@
 
 from datetime import datetime
-from fastapi import APIRouter, HTTPException
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Query
 from app.schemas.time_slots import TimeSlot
 
 router = APIRouter(
@@ -8,7 +9,7 @@ router = APIRouter(
     tags=['Slots']
 )
 
-SLOTS_DB: set[TimeSlot] = set()
+SLOTS_DB: dict[int, TimeSlot] = dict()
 
 @router.get('', response_model=list[TimeSlot])
 async def get_slots():
@@ -16,13 +17,13 @@ async def get_slots():
     Get all time slots.
     '''
 
-    return SLOTS_DB
+    return SLOTS_DB.values()
 
 @router.post('', response_model=TimeSlot, status_code=201)
 async def create_slot(
     start_time: datetime,
     end_time: datetime
-):
+) -> TimeSlot:
     '''
     Create a new time slot
     '''
@@ -37,6 +38,32 @@ async def create_slot(
         is_booked = False,
         booked_by = None 
     )
-    SLOTS_DB.add(new_tslot)
+    SLOTS_DB[slot_id] = new_tslot
 
     return new_tslot
+
+@router.patch('', response_model=TimeSlot)
+async def book_slot(
+    id: int,
+    booked_by: Annotated[str, Query(min_length=3)]) -> TimeSlot:
+    '''
+    Book a time slot
+    '''
+
+    check_if_slots_exists(id, SLOTS_DB)
+    
+    SLOTS_DB[id].booked_by = booked_by
+    SLOTS_DB[id].is_booked = True
+
+    return SLOTS_DB[id]
+
+@router.delete('', status_code=204)
+async def remove_timeslot(id: int):
+    check_if_slots_exists(id, SLOTS_DB)
+
+    SLOTS_DB.pop(id, None)
+
+def check_if_slots_exists(id: int, db: dict[int, TimeSlot]):
+    if not id in db:
+        raise HTTPException(status_code=404
+            ,detail='No such time slot available')
--- app/schemas/time_slots.py
+++ app/schemas/time_slots.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, model_validator, Field
 from datetime import datetime
 from typing import Optional
 
@@ -7,14 +7,14 @@ class TimeSlot(BaseModel):
     start_time: datetime
     end_time: datetime
     is_booked : bool = False
-    booked_by : Optional[datetime] = None
+    booked_by : str | None = Field(min_length=3, default=None)
 
     @model_validator(mode='after')
     def validate_range(self) -> "TimeSlot":
         if self.end_time < self.start_time:
             raise ValueError('The start_date must precede the end_date')
         return self
-
+        
     def __hash__(self):
         return hash((self.id, self.booked_by))
 
