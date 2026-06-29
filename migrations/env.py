14cc6504b5409e87a2b565ac44e30aef49d60457
Anver Gadzhiev <gadzhiev.anver@gmail.com>
2026-06-29 22:04:56 +0300

    failed attempt to set up alembic

--- migrations/env.py
+++ migrations/env.py
@@ -4,9 +4,10 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-
+from sqlalchemy import URL, make_url
 from alembic import context
-
+from app.models.timeslots import Base
+import os
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -65,11 +66,36 @@ async def run_async_migrations() -> None:
 
     """
 
+    db_url_parts = [
+    os.getenv('POSTGRES_HOST'),
+    os.getenv('POSTGRES_PORT'),
+    os.getenv('POSTGRES_USER'),
+    os.getenv('POSTGRES_PASSWORD'),
+    os.getenv('POSTGRES_DB'),
+    os.getenv('POSTGRES_DRIVER_NAME')
+]
+
+    if all(db_url_parts):
+        sqlalchemy_url = URL.create(
+            drivername=os.getenv('POSTGRES_DRIVER_NAME'),
+            username=os.getenv('POSTGRES_USER'),
+            password=os.getenv('POSTGRES_PASSWORD'),
+            host=os.getenv('POSTGRES_HOST'),
+            port=int(os.getenv('POSTGRES_PORT')),
+            database=os.getenv('POSTGRES_DB'),
+        )
+        stringified_sqlalchemy_url = sqlalchemy_url.render_as_string(
+            hide_password=False
+        )
+        percent_replaced_url = stringified_sqlalchemy_url.replace("%", "%%")
+
+        config.set_main_option('sqlalchemy.url', percent_replaced_url)
+    
     connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+            config.get_section(config.config_ini_section, {}),
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+        )
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
